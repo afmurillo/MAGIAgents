0ef2fe65ed468a3bdd3c2b938493301faa175926
root <root@ubuntu1>
2015-07-02 01:22:40 -0700

    debugged

--- Agents/httpClient/httpClient.py
+++ Agents/httpClient/httpClient.py
@@ -16,9 +16,9 @@ class httpClient(DispatchAgent):
 		#self.server = '10.1.2.1' 
 		self.url = 'http://10.1.2.1/file.html'
 		self.rate = 10.0
-		self.n_requests = 30
-		self.log_file_name = 'http_output_1.txt'
-		self.limit_rate = '1m'
+		self.n_requests = 10
+		self.log_file_name = '/share/magi/modules/MAGIAgents/Agents/httpClient/http_output_2.txt'
+		self.limit_rate = '100m'
 
 	@agentmethod()
 	def startHttp(self, msg):
@@ -26,9 +26,9 @@ class httpClient(DispatchAgent):
 		logfile = open(self.log_file_name, "w")
 		#dig @10.1.2.1 www.flowfence.com
 		for i in range(self.n_requests):
-			command = 'wget -q0 --no-cache --limit-rate=' + str(self.limit_rate) + ' ' + str(self.url) ' > /dev/null'
+			command = 'wget -O /dev/null --no-cache --limit-rate=' + str(self.limit_rate) + ' ' + str(self.url) + ' > /dev/null'
 			print "Command: ", command
-			pid = subprocess.Popen(command, shell=True, stdout=logfile)
+			pid = subprocess.Popen(command, shell=True, stderr=logfile)
 			pid.wait()
 			time.sleep(rate_ms)
 		return True
--- Agents/httpClient/http_test_code.py
+++ Agents/httpClient/http_test_code.py
@@ -2,10 +2,10 @@ import subprocess, os
 import time
 
 #server = '10.1.2.1' 
-url = 'http://www.semana.com/deportes/articulo/wilmar-roldan-pitara-la-final-entre-chile-argentina/433316-3'
+url = 'http://10.1.2.1/file.html'
 rate = 10.0
-n_requests = 30
-log_file_name = 'http_output_1.txt'
+n_requests = 3
+log_file_name = '/share/magi/modules/MAGIAgents/Agents/httpClient/http_output_1.txt'
 limit_rate = '1m'
 
 test_start_time = time.time()
@@ -15,14 +15,13 @@ rate_ms = float(1/rate)
 
 for i in range(n_requests):
 
-	command = 'wget -q0 --no-cache --limit-rate=' + str(limit_rate) + ' ' + str(url)
+	command = 'wget -O /dev/null --no-cache --limit-rate=' + str(limit_rate) + ' ' + str(url)
 	#print "Command: ", command
 	initial_time = time.time()
-	pid = subprocess.Popen(command, shell=True, stdout=log_file)
+	pid = subprocess.Popen(command, shell=True, stderr=log_file)
 	pid.wait()
 	time.sleep(rate_ms)
 	finish_time = time.time() - initial_time
 	#print "finish time ", finish_time
 
 test_end_time = time.time() - test_start_time
-print "Total test time", test_end_time
