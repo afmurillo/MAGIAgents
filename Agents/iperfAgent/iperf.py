3b43e247ca2a89a3c546ed48121efabf6b34ebd6
root <root@ubuntu1>
2016-08-31 11:41:09 -0700

    For some reason, the version available at github was non functional. It appears that I never did push from the vm images.

--- Agents/iperfAgent/iperf.py
+++ Agents/iperfAgent/iperf.py
@@ -9,28 +9,30 @@ def getAgent():
 class iperf(DispatchAgent):
 	def __init__(self):
 		DispatchAgent.__init__(self)
-		self.connect= '10.1.2.1'
+		self.connect= '10.1.2.2'
 
 		# Only used in UDP mode
 		self.bw = '20m'
 		self.format = 'm'
-		self.duration_time = 60
-		self.report_time = 1
+		self.duration_time = '60'
+		self.report_time = '1'
 		self.mode = 'TCP'
+		self.logfile = 'reply_time_1.txt'
 
 	@agentmethod()
 	def startPerf(self, msg):
 		#logfile = open(self.file, "w")
 		if self.mode == 'TCP':
-			self.builtcommand = 'iperf -c ' + self.connect + ' -f ' + self.format + ' -t ' + self.duration_time + ' -i ' + self.report_time
-		else if self.mode == 'UDP'::
+			self.builtcommand = 'iperf -c ' + self.connect + ' -f ' + self.format + ' -t ' + str(self.duration_time) + ' -i ' + str(self.report_time)
+		elif self.mode == 'UDP':
 			self.builtcommand = 'iperf -c ' + self.connect + ' -f ' + self.format + ' -t ' + str(self.duration_time) + ' -i ' + str(self.report_time) + ' -b ' + self.bw
 
 		else:
 			print "Invalid mode!"
 			return False
 
-        self.p = subprocess.Popen(self.builtcommand, shell=True, stdout=logfile)		
-		ret_code = self.p.wait()
+		with open(self.logfile,"w+") as f:
+		        self.p = subprocess.Popen(self.builtcommand, shell=True, stdout=f)		
+			ret_code = self.p.wait()
 		return True
 
